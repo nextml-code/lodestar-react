e447757964c16a5ed84cf8912702a69f215abe3d
Filip Johansson <filip@aiwizo.com>
2022-12-07 14:56:41 +0100

    improve: actions

--- src/ApplicationState/Actions/index.js
+++ src/ApplicationState/Actions/index.js
@@ -3,17 +3,25 @@
 // and map to an update function in ./switch.js
 
 import { curry } from "@nextml/lodestar";
-import { NO_PAYLOAD_ACTION_TYPES, PAYLOAD_ACTION_TYPES } from "./types.js";
+import { ADD, MERGE, REPLACE, TOGGLE } from "./types.js";
 
 const action = curry((type, key, payload) => ({ type, key, payload }));
 const noPayloadAction = curry((type, key) => ({ type, key }));
 
-const construct = (fn) => (actions, actionType) => ({
-  ...actions,
-  [actionType]: fn(actionType),
-});
+// const construct = (fn) => (actions, actionType) => ({
+//   ...actions,
+//   [actionType]: fn(actionType),
+// });
 
-export const Actions = PAYLOAD_ACTION_TYPES.reduce(
-  construct(action),
-  NO_PAYLOAD_ACTION_TYPES.reduce(construct(noPayloadAction), {})
-);
+// export const Actions = PAYLOAD_ACTION_TYPES.reduce(
+//   construct(action),
+//   NO_PAYLOAD_ACTION_TYPES.reduce(construct(noPayloadAction), {})
+// );
+
+export const Actions = {
+  merge: action(MERGE),
+  add: action(ADD),
+  replace: action(REPLACE),
+
+  toggle: noPayloadAction(TOGGLE),
+};
--- src/ApplicationState/Actions/merge.js
+++ src/ApplicationState/Actions/merge.js
@@ -1,15 +1,15 @@
-import { typeOf } from "@nextml/lodestar";
+import { curry, typeOf } from "@nextml/lodestar";
 
-export const merge = (x, y) => {
+export const merge = curry((x, y) => {
   switch (typeOf(x)) {
     case "array": {
-      return [...x, ...y];
+      return [...y, ...x];
     }
     case "object": {
-      return { ...x, ...y };
+      return { ...y, ...x };
     }
     default: {
       throw new TypeError(`merge is not implemented for type ${typeOf(x)}`);
     }
   }
-};
+});
